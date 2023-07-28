5051593a6495d6061b5b8df90b1daee7f2f73304
Almazatun <atunstyle@gmail.com>
2023-07-28 11:34:54 +0600

    simple update

--- pkg/http/routes.go
+++ pkg/http/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"path/filepath"
+
 	"github.com/Almazatun/qrcode-iod/pkg/http/handler"
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,8 +12,14 @@ func PubRoutes(a *fiber.App) {
 	// Create routes group.
 	route := a.Group("/")
 
+	absPath, err := filepath.Abs("pkg/http/static/index.html")
+
+	if err != nil {
+		panic(err)
+	}
+
 	// Serve the static index.html file when the root URL is requested
-	route.Static("", "pkg/http/static/index.html")
+	route.Static("", absPath)
 
 	//QR code handler
 	qrcodeHandler := handler.QRCodeHandlerInstance{}
